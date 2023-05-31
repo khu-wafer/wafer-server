f9dccffaccd56887a18cd8e2d1f636a7945da18f
L2HYUNN <donghyuni98@gmail.com>
2023-06-01 04:24:11 +0900

    chore(login): remove login module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoginModule } from './login/login.module';
 import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
 import { WarehouseModule } from './warehouse/warehouse.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), LoginModule, WarehouseModule],
+  imports: [ConfigModule.forRoot(), WarehouseModule],
   controllers: [AppController],
   providers: [AppService],
 })
