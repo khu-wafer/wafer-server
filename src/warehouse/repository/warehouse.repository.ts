8e4976e392a4da63ddeaa19caedf459d9fd859e6
L2HYUNN <donghyuni98@gmail.com>
2023-06-01 06:37:57 +0900

    feat(warehouse): add warehouse repository create method

--- src/warehouse/repository/warehouse.repository.ts
+++ src/warehouse/repository/warehouse.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Warehouse } from '../warehouse.schema';
 import { Model } from 'mongoose';
+import { WarehouseResponseDto } from '../dto/response-warehouse.dto';
 
 @Injectable()
 export class WarehouseRepository {
@@ -9,4 +10,8 @@ export class WarehouseRepository {
     @InjectModel(Warehouse.name)
     private readonly warehouseModel: Model<Warehouse>,
   ) {}
+
+  async create(warehouse: WarehouseResponseDto): Promise<Warehouse> {
+    return await this.warehouseModel.create(warehouse);
+  }
 }
