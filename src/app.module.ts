2ff2893e5ecc5436ed1c21bff86d1d734e8bee72
L2HYUNN <donghyuni98@gmail.com>
2023-06-01 05:20:31 +0900

    fix(mongoose): add mongoose configuration mongoose module for root

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
 import { WarehouseModule } from './warehouse/warehouse.module';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WarehouseModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    WarehouseModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
