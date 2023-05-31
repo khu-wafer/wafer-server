fc847c0ce2eb9136b05757c44c66de36a0089445
L2HYUNN <donghyuni98@gmail.com>
2023-06-01 05:21:12 +0900

    feat(user): add user signup service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserRepository } from './repository/user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, UserRepository],
+  exports: [UserService, UserRepository],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserRepository } from './repository/user.repository';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async signUp(body: CreateUserDto) {
+    const { email, password } = body;
+    const isUserExists = await this.userRepository.existsByEmail(email);
+
+    if (isUserExists)
+      throw new UnauthorizedException('해당하는 유저가 이미 존재합니다.');
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userRepository.create({
+      email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
