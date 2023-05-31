d50669d7201d1449475e986434c7f899821beb9f
L2HYUNN <donghyuni98@gmail.com>
2023-06-01 04:59:33 +0900

    feat(user): add user repository

---
+++ src/user/repository/user.repository.ts
@@ -0,0 +1,25 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from '../user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from '../dto/create-user.dto';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async existsByEmail(email: string): Promise<boolean> {
+    try {
+      const result = await this.userModel.exists({ email });
+      return !!result;
+    } catch (error) {
+      throw new HttpException('User Not Found', 400);
+    }
+  }
+
+  async create(user: CreateUserDto): Promise<User> {
+    return await this.userModel.create(user);
+  }
+}
