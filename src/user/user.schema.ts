acf9dd863cbcd09dd2a1647891c1c72ab68b746b
L2HYUNN <donghyuni98@gmail.com>
2023-06-01 05:07:07 +0900

    fix(user): add user schema document import

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -1,5 +1,6 @@
 import { Schema, SchemaFactory, SchemaOptions } from '@nestjs/mongoose';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Document } from 'mongoose';
 
 const options: SchemaOptions = {
   timestamps: true,
